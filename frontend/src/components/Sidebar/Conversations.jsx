a07fc741476e1dc4d6ae8ec803458d02c3ac1bbf
Vipul Tyagi <vipultyagi52444@gmail.com>
2024-07-23 01:00:20 +0530

    UI done 2nd part

---
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      <Conversation/>
+      
+
+    </div>
+  )
+}
+
+export default Conversations
+
+
+
+// import React from 'react'
+// import Conversation from './Conversation'
+
+// const Conversations = () => {
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto'>
+//       <Conversation/>
+//       <Conversation/>
+//       <Conversation/>
+//       <Conversation/>
+//       <Conversation/>
+//       <Conversation/>
+      
+
+//     </div>
+//   )
+// }
+
+// export default Conversations
